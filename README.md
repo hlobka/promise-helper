806d9af6201bb4d84fa012a0f6de7a243c4b39df
hlib.savenko <hlib.savenko@sigma.software>
2023-10-30 03:02:29 +0200

    #1 | Added example

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # promise-helper
 promise helper
+
+## Installation:
+As a dependency in your npm package:
+- `npm install @hlobka/promise-helper`
+
+Examples:
+- Example 1:
+    - ```typescript
+      new PromiseList<void>([
+        Promise.resolve(),
+        Promise.resolve()
+      ]).all()
+        .then(() => {
+          console.log("Hello World");
+        });
+    ```
--- src/hlobka/promise/PromiseList.ts
+++ src/hlobka/promise/PromiseList.ts
@@ -1,12 +1,12 @@
 export default class PromiseList<T> {
-    constructor(protected promises:Array<Promise<T>>) {
+    constructor(protected promises: Array<Promise<T>>) {
     }
 
-    all():Promise<T[]> {
+    all(): Promise<T[]> {
         return Promise.all(this.promises);
     }
 
-    race():Promise<T> {
+    race(): Promise<T> {
         return Promise.race(this.promises);
     }
 }
